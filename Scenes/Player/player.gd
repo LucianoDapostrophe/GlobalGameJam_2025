bbfffe01cc1aedf6152f612302a1586d4e5799f6
ErikBGameDesign <erikbgamedesign@gmail.com>
2025-01-20 20:25:33 -0800

    Implemented character movement
    Orthogonal character movement works now

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -11,12 +11,16 @@ func _process(_delta):
 # Carp faces the direction that is pressed
 	if Input.is_action_pressed("ui_right"):
 		$CarpSpriteFrames.play("CarpWalkRight")
+
 	if Input.is_action_pressed("ui_left"):
 		$CarpSpriteFrames.play("CarpWalkLeft")
+
 	if Input.is_action_pressed("ui_up"):
 		$CarpSpriteFrames.play("CarpWalkUp")
+
 	if Input.is_action_pressed("ui_down"):
 		$CarpSpriteFrames.play("CarpWalkDown")
+
 # Line up idle animations with direction that was just released
 	if Input.is_action_just_released("ui_right"):
 		$CarpSpriteFrames.play("CarpIdleRight")
@@ -29,9 +33,22 @@ func _process(_delta):
 # ===DIRECTIONAL INPUT===
 
 func _physics_process(_delta):
+#Reset Velocity
+	print ("Velocity:", velocity)
+	velocity = Vector2.ZERO
+#Character Movement
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = speed
+		velocity.x -= 1
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = -speed
+		velocity.x += 1
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
 	else:
 		velocity.x = 0
+		# Normalize velocity to maintain consistent speed when moving diagonally
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * speed
+		
+	move_and_slide()
