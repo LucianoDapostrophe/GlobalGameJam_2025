a0b55048d2d02c2ca6abbd9047d6e360b350caf8
ErikBGameDesign <erikbgamedesign@gmail.com>
2025-01-20 16:08:56 -0800

    Resolved all Errors
    Resolved scripting errors in the player scene

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,35 +1,37 @@
-extends AnimationPlayer
+extends CharacterBody2D
 
 # Variables
 var speed = 200
 
 func _ready() -> void:
-	$AnimationPlayer/CarpSpriteFrames.play("CarpIdleRight")  # Start with the "CarpIdleRight" animation
+	$CarpSpriteFrames.play("CarpIdleRight")  # Start with the "CarpIdleRight" animation
 
 func _process(_delta):
 # ===ANIMATIONS===
 # Carp faces the direction that is pressed
 	if Input.is_action_pressed("ui_right"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpWalkRight")
+		$CarpSpriteFrames.play("CarpWalkRight")
 	if Input.is_action_pressed("ui_left"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpWalkLeft")
+		$CarpSpriteFrames.play("CarpWalkLeft")
 	if Input.is_action_pressed("ui_up"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpWalkUp")
+		$CarpSpriteFrames.play("CarpWalkUp")
 	if Input.is_action_pressed("ui_down"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpWalkDown")
+		$CarpSpriteFrames.play("CarpWalkDown")
 # Line up idle animations with direction that was just released
 	if Input.is_action_just_released("ui_right"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpIdleRight")
+		$CarpSpriteFrames.play("CarpIdleRight")
 	if Input.is_action_just_released("ui_left"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpIdleLeft")
+		$CarpSpriteFrames.play("CarpIdleLeft")
 	if Input.is_action_just_released("ui_up"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpIdleUp")
+		$CarpSpriteFrames.play("CarpIdleUp")
 	if Input.is_action_just_released("ui_down"):
-		$AnimationPlayer/CarpSpriteFrames.play("CarpIdleDown")
+		$CarpSpriteFrames.play("CarpIdleDown")
 # ===DIRECTIONAL INPUT===
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = speed
 	if Input.is_action_pressed("ui_left"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = -speed
+	else:
+		velocity.x = 0
