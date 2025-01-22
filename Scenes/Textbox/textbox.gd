ba3498a13c243edbffd1e2b2d5a8aa3456f79dea
LucianoDapostrophe <90058982+LucianoDapostrophe@users.noreply.github.com>
2025-01-21 17:13:33 -0700

    Button to skip and advance the text

--- Scenes/Textbox/textbox.gd
+++ Scenes/Textbox/textbox.gd
@@ -8,9 +8,32 @@ const READ_RATE = 0.05
 @onready var msg_text = $TextboxContainer/MarginContainer/HBoxContainer/Text
 @onready var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT)
 
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+
+var current_state = State.READY
+
 func _ready():
+	print("Starting state ready")
 	hide_textbox()
 	add_text("This text is going to be added")
+	
+func _process(delta: float):
+	match current_state:
+		State.READY:
+			pass
+		State.READING:
+			if Input.is_action_just_pressed("ui_accept"):
+				tween.stop()
+				msg_text.visible_ratio = 1.0
+				on_tween_finished()
+		State.FINISHED:
+			if Input.is_action_just_pressed("ui_accept"):
+				change_state(State.READY)
+				hide_textbox()
 	
 func hide_textbox():
 	start_symbol.text = ""
@@ -25,5 +48,22 @@ func show_textbox():
 func add_text(next_text):
 	msg_text.text = next_text
 	msg_text.visible_ratio = 0.0
+	change_state(State.READING)
 	show_textbox()
+	tween.connect("finished", on_tween_finished)
 	tween.tween_property(msg_text, "visible_ratio", 1.0, len(next_text) * READ_RATE)
+	
+func on_tween_finished():
+	end_symbol.text = "V"
+	change_state(State.FINISHED)
+	
+func change_state(next_state):
+	current_state = next_state
+	match current_state:
+		State.READY:
+			print("Changing state to ready")
+		State.READING:
+			print("Changing state to reading")
+		State.FINISHED:
+			print("Changing state to finished")
+		
