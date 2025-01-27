b26478dc68370c472ddc53c08bdbef3e15eb0ee3
LucianoDapostrophe <90058982+LucianoDapostrophe@users.noreply.github.com>
2025-01-26 18:21:38 -0700

    Initial cat for battle scene

---
+++ Scenes/Battle/BattleSceneCat/battle_scene_cat.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+const MAX_HUNGER = 100
+var hunger : int
+var aversion : int
+var hyperactivity : float
+var name
+
+func _ready() -> void:
+	pass
+	
+func on_wait_selected():
+	pass
+
+func on_kneel_selected():
+	pass
+
+func on_pspspsps_selected():
+	pass
+	
+func on_extend_hand_selected():
+	pass
+	
+func on_laser_selected():
+	pass
+	
+func on_pet_selected():
+	pass
+	
+func on_food_selected():
+	pass
+	
+func on_toy_mouse_selected():
+	pass
+	
+func on_water_selected():
+	pass
+
+func on_yarn_selected():
+	pass
+
+func on_catnip_selected():
+	pass
+	
+func response_finished():
+	pass
